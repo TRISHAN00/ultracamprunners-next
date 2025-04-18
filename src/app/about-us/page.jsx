af68b405bc0f88d535665d2e269199b916121927
TRISHAN00 <trishansaha43@gmail.com>
2025-04-19 01:47:59 +0600

    add banner section in the about page

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,9 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
+import InnerBanner from "../components/InnerBanner";
 import About from "../components/home/About";
 
 export default function AboutUs() {
-  const [data, setData] = useState(null);
+  const [aboutData, setAboutData] = useState(null);
+  const [bannerData, setBannerData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
@@ -11,11 +13,17 @@ export default function AboutUs() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await fetch(
-          `${API_BASE_URL}/get-req-data/sections?type=slug&value=home&get_section=yes&image=yes&post=yes&file=no&gallery=no`
-        );
-        const result = await res.json();
-        setData(result);
+        // Call both APIs simultaneously
+        const [aboutRes, bannerRes] = await Promise.all([
+          fetch(`${API_BASE_URL}/get-req-data/sections?type=slug&value=home&get_section=yes&image=yes&post=yes&file=no&gallery=no`),
+          fetch(`${API_BASE_URL}/get-req-data/sections?type=slug&value=about-us-page&get_section=yes&image=yes&post=no&file=no&gallery=no`)
+        ]);
+
+        const aboutResult = await aboutRes.json();
+        const bannerResult = await bannerRes.json();
+
+        setAboutData(aboutResult);
+        setBannerData(bannerResult);
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -34,13 +42,21 @@ export default function AboutUs() {
     );
   }
 
-  const aboutUs = data?.data?.sections?.find(
+  const aboutUs = aboutData?.data?.sections?.find(
     (f) => f?.section_data?.slug === "about-us"
   );
 
+  const innerBanner = bannerData?.data?.sections?.find(
+    (f) => f?.section_data?.slug === "about-banner"
+  );
+
   return (
-    <div className="pt-[120px] pb-[120px]">
+    <>
+      <InnerBanner
+        title={innerBanner?.section_data?.subtitle}
+        img={innerBanner?.images?.list?.[0]?.full_path}
+      />
       <About data={aboutUs} />
-    </div>
+    </>
   );
 }
