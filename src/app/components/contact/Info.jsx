af7ef065eaad02bf66a49ec80a260222d6d6e6a3
TRISHAN00 <trishansaha43@gmail.com>
2025-05-29 00:36:10 +0600

    fix the contact page

--- src/app/components/contact/Info.jsx
+++ src/app/components/contact/Info.jsx
@@ -1,7 +1,8 @@
-'use client'
+"use client";
 
-import parse from 'html-react-parser';
-import Link from 'next/link';
+import parse from "html-react-parser";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Info({ info }) {
   return (
@@ -10,35 +11,39 @@ export default function Info({ info }) {
       <div className="w-full bg-white my-6 md:my-8 lg:my-10 py-6 px-4 md:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-center md:text-left">
-            {
-              info?.posts?.list?.map((item, index) => {
-                const title = item?.data?.title || 'No title';
-                const description = item?.data?.description || '';
+            {info?.posts?.list?.map((item, index) => {
+              const title = item?.data?.title || "No title";
+              const description = item?.data?.description || "";
+              const icon = item?.images?.[0]?.full_path || "";
 
-                return (
-                  <Link
-                    key={item?.id || index}
-                    href={'/'}
-                    className="group flex flex-col md:flex-row items-center gap-3 transition-colors hover:text-[#AD242F]"
-                  >
-                    <a>
-                      <div className="p-2 rounded-full bg-[#AD242F] text-white group-hover:bg-[#AD242F]/90">
-                      icon
-                    </div>
-                    <div>
-                      <h3 className="font-semibold text-lg text-[#AD242F]">{title}</h3>
-                      <p className="text-muted-foreground group-hover:text-[#AD242F]">
-                        {parse(description)}
-                      </p>
-                    </div>
-                    </a>
-                  </Link>
-                )
-              })
-            }
+              return (
+                <Link
+                  key={item?.id || index}
+                  href={"/"}
+                  className="group flex flex-col md:flex-row items-center gap-3 transition-colors "
+                >
+                  <div className="p-2 rounded-full  text-white">
+                    <Image
+                      src={icon}
+                      alt={title}
+                      width={30} // or adjust size as needed
+                      height={30}
+                    />
+                  </div>
+                  <div>
+                    <h3 className="font-semibold text-lg text-[#AD242F]">
+                      {title}
+                    </h3>
+                    <p className="text-muted-foreground group-hover:text-[#AD242F]">
+                      {parse(description)}
+                    </p>
+                  </div>
+                </Link>
+              );
+            })}
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
